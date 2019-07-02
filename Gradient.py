43d8d84029d2f17af8b3728d93b6255a5f10f120
Oskar Tengberg <52031625+MrTengil@users.noreply.github.com>
2019-07-02 16:15:51 +0200

    Finalized the program
    Post-processing
    Header for all the functions

--- Gradient.py
+++ Gradient.py
@@ -1,9 +1,23 @@
+'''
+    Written by: Oskar Tengberg, 2019
+                https://github.com/MrTengil
+
+    This function generates the gradient of a function "f" at position "x" using the forward difference method.
+    [Input]
+        f = function
+        x = point (1D-array)
+        ab = dimension of the problem (integer)
+    [Output]
+        G = gradient (1D-array)
+
+'''
+
 import numpy as np
 
 def Gradient(f, x, ab):
     h = np.finfo(float).eps**0.5    # Alter the small step h from machine epsilon
     e = np.eye(ab, ab)
-    G = np.zeros((ab, 1),float)     # Initiate the Gradient vector
+    G = np.zeros((ab, 1), float)    # Initiate the Gradient vector
     for i in range(ab):
-        G[i] = (f(x + h*e[:,i]) - f(x))/h
+        G[i] = (f(x + h*e[:, i]) - f(x))/h  # Compute the gradient array
     return G
